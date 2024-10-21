312d47831132674ffbaee610e1f14d411525c880
Harold Cooper <hrldcpr@gmail.com>
2024-10-21 11:35:26 -0400

    download script

---
+++ download.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import time
+
+skip = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+
+for i, line in enumerate(open("urls.txt")):
+    if i < skip:
+        continue
+    subprocess.run(["wget", "-qx", line.strip()]).check_returncode()
+    print(i)
+    time.sleep(0.5)
