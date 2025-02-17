1c5b633c3d54b445c2e3d80022abcd95214bed4b
Harold Cooper <hrldcpr@gmail.com>
2025-02-17 12:30:53 -0500

    clearer wget args

--- download.py
+++ download.py
@@ -7,6 +7,8 @@ skip = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 for i, line in enumerate(open("urls.txt")):
     if i < skip:
         continue
-    subprocess.run(["wget", "-qx", line.strip()]).check_returncode()
+    subprocess.run(
+        ["wget", "--quiet", "--force-directories", line.strip()]
+    ).check_returncode()
     print(i)
     time.sleep(0.5)
